57873353a0ea88830728043dfc4d7e73871fcbd5
Shushanth <b23ai101@kitsw.ac.in>
2025-10-30 22:31:25 +0530

    added executeCode fn in /src/api.js

--- src/api.js
+++ src/api.js
@@ -5,13 +5,13 @@ export const API = axios.create({
     baseURL:"https://emkc.org/api/v2/piston",
 })
 
-export const executeCode = async (language,sourceCode)=>{
+export const executeCode = async (language,code)=>{
     const res = await API.post("/execute",{
         "language":language,
         "version":LANGUAGES[language].version,
         "files":[
             {
-                "content":sourceCode
+                "content":code
             }
         ]
     })
