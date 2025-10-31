3839eeaa02eef32b1dd6e586562dff9fa499bcba
Shushanth <b23ai101@kitsw.ac.in>
2025-10-31 22:48:05 +0530

    removed unused imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useRef, useState } from "react";
-import CodePanel from "./components/CodePanel";
-import OutputPanel from "./components/OutputPanel";
 import EditorPage from "./components/EditorPage";
 
 export default function App() {
