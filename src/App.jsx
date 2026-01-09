a026d5d3fd2a258c2febd37c9a7f381b455f1d03
Shushanth <b23ai101@kitsw.ac.in>
2026-01-09 19:46:19 +0530

    removed minor mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { defaultCodeSnippets} from './constants';
+import { defaultCodeSnippets } from './constants';
 import LeftPanel from './components/LeftPanel';
 import RightPanel from './components/RightPanel';
 
@@ -14,8 +14,8 @@ function App() {
   const [code, setCode] = useState(
     defaultCodeSnippets["javascript"].code
   );
-  const [output,setOutput] = useState([]);
-  const [loading,setLoading] = useState(false);
+  const [output, setOutput] = useState([]);
+  const [loading, setLoading] = useState(false);
   const editorRef = useRef(null);
 
   const getRunTimes = async () => {
@@ -32,38 +32,40 @@ function App() {
 
 
   useEffect(() => {
-    getRunTimes();
+    getRunTimes()
+
   }, [])
 
-  const executeCode = async ()=>{
+  const executeCode = async () => {
     const currentCode = editorRef.current.getValue();
-    setLoading(prev=>!prev);
-    const res = await fetch("https://emkc.org/api/v2/piston/execute",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
+    setLoading(prev => !prev);
+    const res = await fetch("https://emkc.org/api/v2/piston/execute", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
       },
-      body:JSON.stringify({
-  "language": runtime.language,
-  "version": runtime.version,
-  "files": [
-    {
-      "name": defaultCodeSnippets[runtime.language].filename,
-      "content": currentCode
-    }
-  ],
-  "stdin": "",
-  "args": ["1", "2", "3"],
-  "compile_timeout": 10000,
-  "run_timeout": 3000,
-  "compile_memory_limit": -1,
-  "run_memory_limit": -1
-})
+      body: JSON.stringify({
+        "language": runtime.language,
+        "version": runtime.version,
+        "files": [
+          {
+            "name": defaultCodeSnippets[runtime.language].filename,
+            "content": currentCode
+          }
+        ],
+        "stdin": "",
+        "args": ["1", "2", "3"],
+        "compile_timeout": 10000,
+        "run_timeout": 3000,
+        "compile_memory_limit": -1,
+        "run_memory_limit": -1
+      })
     })
 
+
     const data = await res.json();
-    setLoading(prev=>!prev);
-    
+    setLoading(prev => !prev);
+
     setOutput(data.run);
   }
 
@@ -74,7 +76,7 @@ function App() {
       <div className="d-flex" style={{ height: "100vh" }}>
 
         <LeftPanel runtimes={runtimes} runtime={runtime} setRuntime={setRuntime} code={code} setCode={setCode} editorRef={editorRef} executeCode={executeCode} loading={loading} />
-        <RightPanel output={output} loading={loading}/>
+        <RightPanel output={output} loading={loading} />
 
       </div>
     </>
